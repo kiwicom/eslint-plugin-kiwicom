0f95938a6ebfe7f334a8ec4c00f8eda76a96d99d
Ivan <richardivan.com@gmail.com>
2018-12-06 11:24:38 +0100

    feat(npm package): build production package and add step to build production package before releasin

---
+++ webpack.prod.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  context: __dirname,
+  entry: ['./src/index.js'],
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: 'index.js',
+    // // [hash:6] with add a SHA based on file changes if the env is build
+    // filename:
+    //   env === EnvEnum.BUILD ? '[name]-[hash:6].min.js' : '[name].min.js',
+    library: 'eslintPluginKiwicom',
+    libraryTarget: 'umd'
+  },
+  node: {
+    __dirname: true,
+    __filename: true
+  },
+  watch: false,
+  mode: 'production',
+  target: 'node',
+  externals: [
+    nodeExternals({
+      whitelist: ['ramda']
+    })
+    // nodeExternals({
+    //   whitelist: ['webpack/hot/poll?300']
+    // })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  recordsPath: path.join(__dirname, 'dist/_records'),
+  plugins: [
+    new webpack.IgnorePlugin(/\.(css|less)$/),
+    new webpack.BannerPlugin({
+      banner: 'require("source-map-support").install();',
+      raw: true,
+      entryOnly: false
+    })
+    // new webpack.HotModuleReplacementPlugin({ quiet: true })
+  ]
+}
