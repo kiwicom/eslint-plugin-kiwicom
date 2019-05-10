2fbfaec653c1f2e613cc7e3b2fd32e5a6daccaed
Ivan <richardivan.com@gmail.com>
2019-05-10 12:28:24 +0200

    initial commit for nitro Translate rule

--- src/rules/index.js
+++ src/rules/index.js
@@ -16,11 +16,13 @@ import { preferNitroTextComponent } from './orbit/prefer-nitro-text-component'
 import { preferNitroTranslateComponent } from './nitro/prefer-nitro-translate-component'
 import { noTkeyNitroTranslateComponent } from './nitro/no-tkey-nitro-translate-component'
 import { nitroUseStringForNonDynamicTranslationKeys } from './nitro/nitro-use-string-for-non-dynamic-translation-keys'
+import { nitroTranslate } from './nitro/nitro-translate'
 
 export const rules = {
   'orbit-text-component-name': orbitTextComponentName,
   'prefer-nitro-text-component': preferNitroTextComponent,
   'prefer-nitro-translate-component': preferNitroTranslateComponent,
   'no-tkey-nitro-translate-component': noTkeyNitroTranslateComponent,
-  'nitro-use-string-for-non-dynamic-translation-keys': nitroUseStringForNonDynamicTranslationKeys
+  'nitro-use-string-for-non-dynamic-translation-keys': nitroUseStringForNonDynamicTranslationKeys,
+  'nitro-translate': nitroTranslate
 }
